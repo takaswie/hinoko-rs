f35bbb949fea54cc93e888c2ad9fb01182545632
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-05-14 22:43:30 +0900

    hinoko: remove useless subclass
    FwIsoResourceAutoImpl and FwIsoResourceOnceImpl are blank trait, therefore
    useless.
    
    This commit remove them.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- hinoko/src/subclass/fw_iso_resource_auto.rs
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-use super::*;
-
-pub trait FwIsoResourceAutoImpl: FwIsoResourceImpl {}
-
-unsafe impl<T: FwIsoResourceAutoImpl> IsSubclassable<T> for FwIsoResourceAuto {}
--- hinoko/src/subclass/fw_iso_resource_once.rs
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-use super::*;
-
-pub trait FwIsoResourceOnceImpl: FwIsoResourceImpl {}
-
-unsafe impl<T: FwIsoResourceOnceImpl> IsSubclassable<T> for FwIsoResourceOnce {}
