102e25e59a9169ff639d686ec352a38c04454f1b
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-07-14 18:09:57 +0900

    hinoko: make glib crate public inner hinoko crate for documentation
    The documentation includes some references to elements in glib crate and
    it seems to require public usage of glib crate, while it's not preferable
    to expose glib from hinoko crate.
    
    This commit uses 'pub(crate) use' statement for the purpose.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- hinoko/src/lib.rs
+++ hinoko/src/lib.rs
@@ -26,7 +26,7 @@ pub mod subclass;
 pub use ffi;
 
 // For links in documentation.
-use glib;
+pub(crate) use glib;
 
 use glib::{signal::*, translate::*, Cast, Error, IsA, SignalHandlerId, StaticType, Value};
 
