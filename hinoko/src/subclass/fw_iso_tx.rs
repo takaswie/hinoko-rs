takaswie/hinoko-rs

commit bc2306bf932711d7f35a0bb0fd84ef59d9902f6b

hinoko/hinoko-sys: regenerate according to v0.7
As well as subclass support.

Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

hinoko/src/subclass/fw_iso_tx.rs
@@ -1,0 +1,71 @@
+// SPDX-License-Identifier: MIT
+
+use super::*;
+
+pub trait FwIsoTxImpl: FwIsoCtxImpl {
+    fn interrupted(&self, ctx: &Self::Type, sec: u32, cycle: u32, header: &[u8], count: u32) {
+        self.parent_interrupted(ctx, sec, cycle, header, count)
+    }
+}
+
+pub trait FwIsoTxImplExt: ObjectSubclass {
+    fn parent_interrupted(&self, ctx: &Self::Type, sec: u32, cycle: u32, header: &[u8], count: u32);
+}
+
+impl<T: FwIsoTxImpl> FwIsoTxImplExt for T {
+    fn parent_interrupted(
+        &self,
+        ctx: &Self::Type,
+        sec: u32,
+        cycle: u32,
+        header: &[u8],
+        count: u32,
+    ) {
+        unsafe {
+            let data = T::type_data();
+            let parent_class = data.as_ref().parent_class() as *mut ffi::HinokoFwIsoTxClass;
+            let f = (*parent_class)
+                .interrupted
+                .expect("No parent \"interrupted\" implementation");
+
+            f(
+                ctx.unsafe_cast_ref::<FwIsoTx>().to_glib_none().0,
+                sec.into(),
+                cycle.into(),
+                header.as_ptr(),
+                header.len() as u32,
+                count.into(),
+            )
+        }
+    }
+}
+
+unsafe impl<T: FwIsoTxImpl> IsSubclassable<T> for FwIsoTx {
+    fn class_init(class: &mut Class<Self>) {
+        Self::parent_class_init::<T>(class);
+
+        let klass = class.as_mut();
+        klass.interrupted = Some(fw_iso_rx_single_interrupted::<T>);
+    }
+}
+
+unsafe extern "C" fn fw_iso_rx_single_interrupted<T: FwIsoTxImpl>(
+    ctx: *mut ffi::HinokoFwIsoTx,
+    sec: c_uint,
+    cycle: c_uint,
+    header: *const u8,
+    header_length: c_uint,
+    count: c_uint,
+) {
+    let instance = &*(ctx as *mut T::Instance);
+    let imp = instance.imp();
+    let wrap: Borrowed<FwIsoTx> = from_glib_borrow(ctx);
+
+    imp.interrupted(
+        wrap.unsafe_cast_ref(),
+        sec.into(),
+        cycle.into(),
+        std::slice::from_raw_parts(header, header_length as usize),
+        count.into(),
+    )
+}

hinoko/src/auto/fw_iso_tx.rs
@@ -1,4 +1,5 @@
 // This file was generated by gir (https://github.com/gtk-rs/gir)
+// from
 // from gir-files (https://github.com/gtk-rs/gir-files)
 // DO NOT EDIT
 
@@ -11,7 +12,7 @@ use std::ptr;
 
 glib::wrapper! {
     #[doc(alias = "HinokoFwIsoTx")]
-    pub struct FwIsoTx(Object<ffi::HinokoFwIsoTx, ffi::HinokoFwIsoTxClass>) @extends FwIsoCtx;
+    pub struct FwIsoTx(Object<ffi::HinokoFwIsoTx, ffi::HinokoFwIsoTxClass>) @implements FwIsoCtx;
 
     match fn {
         type_ => || ffi::hinoko_fw_iso_tx_get_type(),
@@ -49,15 +50,6 @@ pub trait FwIsoTxExt: 'static {
         maximum_bytes_per_payload: u32,
         payloads_per_buffer: u32,
     ) -> Result<(), glib::Error>;
-
-    #[doc(alias = "hinoko_fw_iso_tx_release")]
-    fn release(&self);
-
-    #[doc(alias = "hinoko_fw_iso_tx_stop")]
-    fn stop(&self);
-
-    #[doc(alias = "hinoko_fw_iso_tx_unmap_buffer")]
-    fn unmap_buffer(&self);
 }
 
 impl<O: IsA<FwIsoTx>> FwIsoTxExt for O {
@@ -70,7 +62,7 @@ impl<O: IsA<FwIsoTx>> FwIsoTxExt for O {
     ) -> Result<(), glib::Error> {
         unsafe {
             let mut error = ptr::null_mut();
-            let _ = ffi::hinoko_fw_iso_tx_allocate(
+            let is_ok = ffi::hinoko_fw_iso_tx_allocate(
                 self.as_ref().to_glib_none().0,
                 path.to_glib_none().0,
                 scode.into_glib(),
@@ -78,6 +70,7 @@ impl<O: IsA<FwIsoTx>> FwIsoTxExt for O {
                 header_size,
                 &mut error,
             );
+            assert_eq!(is_ok == glib::ffi::GFALSE, !error.is_null());
             if error.is_null() {
                 Ok(())
             } else {
@@ -93,35 +86,18 @@ impl<O: IsA<FwIsoTx>> FwIsoTxExt for O {
     ) -> Result<(), glib::Error> {
         unsafe {
             let mut error = ptr::null_mut();
-            let _ = ffi::hinoko_fw_iso_tx_map_buffer(
+            let is_ok = ffi::hinoko_fw_iso_tx_map_buffer(
                 self.as_ref().to_glib_none().0,
                 maximum_bytes_per_payload,
                 payloads_per_buffer,
                 &mut error,
             );
+            assert_eq!(is_ok == glib::ffi::GFALSE, !error.is_null());
             if error.is_null() {
                 Ok(())
             } else {
                 Err(from_glib_full(error))
             }
-        }
-    }
-
-    fn release(&self) {
-        unsafe {
-            ffi::hinoko_fw_iso_tx_release(self.as_ref().to_glib_none().0);
-        }
-    }
-
-    fn stop(&self) {
-        unsafe {
-            ffi::hinoko_fw_iso_tx_stop(self.as_ref().to_glib_none().0);
-        }
-    }
-
-    fn unmap_buffer(&self) {
-        unsafe {
-            ffi::hinoko_fw_iso_tx_unmap_buffer(self.as_ref().to_glib_none().0);
         }
     }
 }

hinoko/src/fw_iso_tx.rs
@@ -9,9 +9,9 @@ pub trait FwIsoTxExtManual {
         header: Option<&[u8]>,
         payload: Option<&[u8]>,
         schedule_interrupt: bool,
-    ) -> Result<(), glib::Error>;
+    ) -> Result<(), Error>;
 
-    fn start(&self, cycle_match: Option<&[u16; 2]>) -> Result<(), glib::Error>;
+    fn start(&self, cycle_match: Option<&[u16; 2]>) -> Result<(), Error>;
 
     fn connect_interrupted<F>(&self, f: F) -> SignalHandlerId
     where
@@ -26,7 +26,7 @@ impl<O: IsA<FwIsoTx>> FwIsoTxExtManual for O {
         header: Option<&[u8]>,
         payload: Option<&[u8]>,
         schedule_interrupt: bool,
-    ) -> Result<(), glib::Error> {
+    ) -> Result<(), Error> {
         let (header_ptr, header_length) = match header {
             Some(h) => (h.as_ptr(), h.len() as u32),
             None => (std::ptr::null(), 0),
@@ -58,7 +58,7 @@ impl<O: IsA<FwIsoTx>> FwIsoTxExtManual for O {
         }
     }
 
-    fn start(&self, cycle_match: Option<&[u16; 2]>) -> Result<(), glib::Error> {
+    fn start(&self, cycle_match: Option<&[u16; 2]>) -> Result<(), Error> {
         unsafe {
             let ptr: *const [u16; 2] = match cycle_match {
                 Some(data) => data,

hinoko-sys/Cargo.toml
@@ -15,6 +15,9 @@ features = ["dox"]
 name = "hinoko"
 version = "0.6"
 
+[package.metadata.system-deps.hinoko.v0_7]
+version = "0.7"
+
 [lib]
 name = "hinoko_sys"
 
@@ -40,3 +43,4 @@ tempfile = "3"
 [features]
 v0_6 = []
 dox = []
+v0_7 = []

hinoko-sys/Gir.toml
@@ -3,7 +3,7 @@ girs_directories = ["../gir-files", ".."]
 work_mode = "sys"
 library = "Hinoko"
 version = "0.0"
-min_cfg_version = "0.6"
+min_cfg_version = "0.7"
 single_version_file = "."
 external_libraries = [
   "GLib",

hinoko-sys/build.rs
@@ -1,4 +1,5 @@
 // This file was generated by gir (https://github.com/gtk-rs/gir)
+// from
 // from gir-files (https://github.com/gtk-rs/gir-files)
 // DO NOT EDIT
 

commit 2588503e7bf01538936fdbd51f297d133de78186

hinoko: fix wrong name of local function for Hinoko.FwIsoTx
Simple copy-and-paste mistake is found at code to make subclass of
Hinoko.FwIsoTx. It is local function thus brings no issue fortunately.

Fixes: bc2306bf9327 ("hinoko/hinoko-sys: regenerate according to v0.7")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

hinoko/src/subclass/fw_iso_tx.rs
@@ -47,11 +47,11 @@ unsafe impl<T: FwIsoTxImpl> IsSubclassable<T> for FwIsoTx {
         Self::parent_class_init::<T>(class);
 
         let klass = class.as_mut();
-        klass.interrupted = Some(fw_iso_rx_single_interrupted::<T>);
+        klass.interrupted = Some(fw_iso_tx_interrupted::<T>);
     }
 }
 
-unsafe extern "C" fn fw_iso_rx_single_interrupted<T: FwIsoTxImpl>(
+unsafe extern "C" fn fw_iso_tx_interrupted<T: FwIsoTxImpl>(
     ctx: *mut ffi::HinokoFwIsoTx,
     sec: c_uint,
     cycle: c_uint,